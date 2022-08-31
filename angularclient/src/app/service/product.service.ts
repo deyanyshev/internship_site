726eaade9228bb1a3364b5d8fc92a140e3d0528e
deyanyshev <dddeyanyshev@gmail.com>
2022-08-31 11:40:18 +0300

    + angular

---
+++ angularclient/src/app/service/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Product } from '../model/product';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  public findAll(): Observable<Product[]> {
+    return this.http.get<Product[]>('http://localhost:8080/products');
+  }
+
+  public save(product: Product) {
+    return this.http.post('http://localhost:8080/add-product', product);
+  }
+}
